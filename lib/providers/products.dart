7ce87540fa853ba4848f1d48122ea017e7b46c27
afiqaimanafr <aaafr98@gmail.com>
2021-12-21 09:54:28 +0800

    cart class added

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/widgets/product_item.dart';
 import './product.dart';
 
 class Products with ChangeNotifier {
