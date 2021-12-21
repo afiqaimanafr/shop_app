b78d2a5e5c277c76012f1795c6930aaf57a750e0
afiqaimanafr <aaafr98@gmail.com>
2021-12-21 10:11:58 +0800

    latest

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/foundation.dart';
 
-class Cart with ChangeNotifier {}
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+}
